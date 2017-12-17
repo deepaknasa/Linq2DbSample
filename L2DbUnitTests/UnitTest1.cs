351c2c820c60df5237c0d4b65c0a81c4ac8b1c89
Deepak Kumar <deepaknasa@gmail.com>
2017-12-17 17:34:07 +1100

    Add project files.

---
+++ L2DbUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L2DbUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
